9cab1922d7ec5e9bb12e7afd3d99e0f0b732d348
mikey-roberts <mikey_roberts@outlook.com>
2021-04-04 20:07:53 +0100

    Update sub_string.rb

--- sub_string/sub_string.rb
+++ sub_string/sub_string.rb
@@ -2,10 +2,8 @@ dictionary = ["plastic","previous","below","low","aggressive","cautious", "trucu
 # takes a string input and returns a hash. K - string V - number of entires  
   def sub_string(string, dictionary)
     string = string.downcase.split(" ")
-    result = {}
-
     merge = string += dictionary
-    merge = merge.tally
-    merge.delete_if {|key, val| val <= 1 }.each { |k, v| result[k] = v - 1 } 
-    return result
+    result = {}
+    merge.tally.delete_if {|key, val| val <= 1 }.each { |k, v| result[k] = v - 1 } 
+    result
   end
